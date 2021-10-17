76bc03422fd71fba7efba6f368dc16768a7bd41c
jleger <joseph.leger@duke-energy.com>
2021-10-17 14:17:12 -0400

    feat: added Select component, broke out Label into its own component, added event masking function for onChange events

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,80 @@
+import { ChangeEvent } from 'react';
+
+// capitalizes the first word in a string
 const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
-const snakeCase = (str: string) => str.split(' ').join('_');
+// masking functions to format inputs during onChange events
+const maskedEvent = {
+  onlyNumbers: (event: ChangeEvent<HTMLInputElement>) => {
+    const maskedValue = event.target.value.replace(/[^0-9]/g, '');
+    event.target.value = maskedValue;
+    return event;
+  },
+};
 
-export { capitalize, snakeCase };
+// to_snake_case
+const snakeCase = (str: string) => str.split(' ').join('_').toLowerCase();
+
+const states = [
+  'AK',
+  'AL',
+  'AR',
+  'AS',
+  'AZ',
+  'CA',
+  'CO',
+  'CT',
+  'DC',
+  'DE',
+  'FL',
+  'FM',
+  'GA',
+  'GU',
+  'HI',
+  'IA',
+  'ID',
+  'IL',
+  'IN',
+  'KS',
+  'KY',
+  'LA',
+  'MA',
+  'MD',
+  'ME',
+  'MH',
+  'MI',
+  'MN',
+  'MO',
+  'MP',
+  'MS',
+  'MT',
+  'NC',
+  'ND',
+  'NE',
+  'NH',
+  'NM',
+  'NJ',
+  'NV',
+  'NY',
+  'OH',
+  'OK',
+  'OR',
+  'PA',
+  'PR',
+  'PW',
+  'RI',
+  'SC',
+  'SD',
+  'TN',
+  'TX',
+  'UT',
+  'VA',
+  'VI',
+  'VT',
+  'WA',
+  'WI',
+  'WV',
+  'WY',
+];
+
+export { capitalize, maskedEvent, snakeCase, states };
