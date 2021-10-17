035c6ae3e571de7ec44505f49fea9bb9dc42e7f6
jleger <joseph.leger@duke-energy.com>
2021-10-17 15:05:29 -0400

    feat: added Modal component and loading state

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,15 +1,19 @@
-import React, { ChangeEvent, FormEvent } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import axios from 'axios';
 import { Button } from '../Button';
 import { FormContextProvider, useFormContext } from '../../contexts/form';
 import { Input } from '../Input';
 import { maskedEvent, states } from '../../utils';
+import { Modal } from '../Modal';
 import { QuoteObjectType } from '../../lambda/types';
 import { Select } from '../Select';
+import { useHistory } from 'react-router';
 import './index.scss';
 
 const FormComponent = () => {
   const { dispatch, state } = useFormContext();
+  const history = useHistory();
+  const [isLoading, setIsLoading] = useState(false);
 
   // save the user data as they type
   const handleChange = ({
@@ -25,50 +29,57 @@ const FormComponent = () => {
   // save the user quote from the api after form submit
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
+    setIsLoading(true);
+
     const { data }: { data: QuoteObjectType } = await axios.post(
       '/.netlify/functions/createQuote',
       state.userInfo
     );
 
     dispatch({ type: 'saveUserQuote', payload: data });
+    setIsLoading(false);
+    data && history.push('/quote-overview');
   };
 
   return (
-    <form
-      className='sure-form'
-      onReset={() => dispatch({ type: 'clearFields' })}
-      onSubmit={handleSubmit}
-    >
-      <Input name='First Name' onChange={handleChange} required />
-      <Input name='Last Name' onChange={handleChange} required />
-      <Input id='line_1' name='Address Line 1' onChange={handleChange} required variant='wide' />
-      <Input id='line_2' name='Address Line 2' onChange={handleChange} variant='wide' />
-      <Input name='City' onChange={handleChange} required />
-      <Select
-        defaultValue=''
-        id='region'
-        name='State'
-        onChange={handleChange}
-        options={states}
-        variant='small'
-        required
-      />
-      <Input
-        id='postal'
-        maxLength={5}
-        name='Zip'
-        onChange={(e) => handleChange(maskedEvent.onlyNumbers(e))}
-        variant='small'
-        required
-      />
-      <Button type='submit' variant='top-margin'>
-        Get your quote!
-      </Button>
-      <Button type='reset' variant='text'>
-        Clear
-      </Button>
-      <div className='sure-form--text-small'>* denotes a required field</div>
-    </form>
+    <>
+      <Modal isActive={isLoading} />
+      <form
+        className='sure-form'
+        onReset={() => dispatch({ type: 'clearFields' })}
+        onSubmit={handleSubmit}
+      >
+        <Input name='First Name' onChange={handleChange} required />
+        <Input name='Last Name' onChange={handleChange} required />
+        <Input id='line_1' name='Address Line 1' onChange={handleChange} required variant='wide' />
+        <Input id='line_2' name='Address Line 2' onChange={handleChange} variant='wide' />
+        <Input name='City' onChange={handleChange} required />
+        <Select
+          defaultValue=''
+          id='region'
+          name='State'
+          onChange={handleChange}
+          options={states}
+          variant='small'
+          required
+        />
+        <Input
+          id='postal'
+          maxLength={5}
+          name='Zip'
+          onChange={(e) => handleChange(maskedEvent.onlyNumbers(e))}
+          variant='small'
+          required
+        />
+        <Button type='submit' variant='top-margin'>
+          Get your quote!
+        </Button>
+        <Button type='reset' variant='text'>
+          Clear
+        </Button>
+        <div className='sure-form--text-small'>* denotes a required field</div>
+      </form>
+    </>
   );
 };
 
