a5d545e665a09921e388fb2c254f85d502aa7a4b
jleger <joseph.leger@duke-energy.com>
2021-10-18 02:14:48 -0400

    fix: cleaned up some responsive styles. Fixed the mixins. Moved Intro into routes directly

--- src/routes/QuoteOverview/index.tsx
+++ src/routes/QuoteOverview/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { IntroOverview } from '../../components/Intro';
+import { Card } from '../../components/Card';
+import { ImageLoader } from '../../components/ImageLoader';
+import { OverViewForm } from '../../components/Forms/OverviewForm';
+import '../index.scss';
 
 const QuoteOverview = () => (
-  <div>
-    <IntroOverview />
+  <div className='sure-intro'>
+    <div className='sure-intro__content'>
+      <h1>Quote Overview</h1>
+      <span>
+        Take a look at your new insurance policy. If you would like to make any changes to your
+        deductible or asteroid collision please feel free to do so
+      </span>
+      <OverViewForm />
+      <Card />
+    </div>
+    <div className='sure-intro--vertical-center'>
+      <ImageLoader
+        alt='man sitting on a chair'
+        aria-hidden={true}
+        className='sure-intro__image'
+        name='man'
+      />
+    </div>
   </div>
 );
 
